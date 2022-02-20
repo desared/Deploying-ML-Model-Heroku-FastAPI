c201067e196ba7304f2dd01691fae729a87e135c
Desared Osmanllari <desared_o@live.com>
2022-02-20 16:49:15 +0100

    run tests

--- test_case/test_process_data.py
+++ test_case/test_process_data.py
@@ -10,4 +10,4 @@ from training.modelling.data import data_cleaning_stage
 def test_clean_data():
     data_cleaning_stage(root_path='./')
 
-    assert os.path.isfile('./data_storage/clean_census.csv')
+    assert os.path.isfile('./data/clean_census.csv')
